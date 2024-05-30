79db68a3630f88de03cc5adff1164a61c36b9659
DSM4 <fatec-dsm4@academico.intra>
2024-05-29 22:11:27 -0300

    ajustes title

--- static/js/Video.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    if (menu.classList.contains("open-menu")) {
-        menu.classList.remove("open-menu");
-    } else {
-        menu.classList.add("open-menu");
-    }
-}
